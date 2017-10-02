76e94d3cab0d1ca2da4378925d04cca312b5925a
dilse07 <32346179+dilse07@users.noreply.github.com>
2017-10-03 09:19:50 +1100

    Initial commit

---
+++ src/main/java/org/mule/customerService/customers/Customer.java
@@ -0,0 +1,42 @@
+package org.mule.customerService.customers;
+
+public class Customer {
+	
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+    	this.firstName = (firstName != null) ? firstName : "";
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = (lastName != null) ? lastName : "";
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = (address != null) ? address : "";
+    }
+  
+}
